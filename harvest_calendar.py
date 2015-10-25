6ae6e1ebb924faf44449041dc23dfef44ebec35f
Zach Carwile <zachary.carwile@gmail.com>
2015-10-25 18:41:16 -0400

    email harvester working and generates text files

--- harvest_calendar.py
+++ harvest_calendar.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import httplib2
 import os
+import time
 
 from apiclient import discovery
 import oauth2client
@@ -19,6 +20,37 @@ SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Calendar API Python Quickstart'
 
+DATA_DIR = '../quixotic/data/operational/calendar'
+
+""" Helper functions: these can probably be shared across all harvesters"""
+
+def get_last_collection_time_and_file_id():
+    
+    # TODO: get last timestamp and associated file id from DATA_DIR  
+        
+    last_id = 0
+    state = None
+    for filename in os.listdir(DATA_DIR):
+        fullpath = os.path.join(DATA_DIR, filename)
+        created = datetime.datetime.fromtimestamp(os.path.getctime(fullpath))
+        
+        if state == None:
+            state = created
+            last_id = int(fullpath.split("zcarwile_")[1].split(".txt")[0])
+        else:
+            if created > state:
+                state = created
+                last_id = int(fullpath.split("zcarwile_")[1].split(".txt")[0])
+
+    if state == None:
+        state = datetime.datetime(2015,9,21).isoformat() + 'Z'
+    else:
+        state = created.isoformat() + 'Z'       
+        #TODO: Need to convert file timestamp to whatever God awful format Google wants
+        pass
+       
+    return state,last_id
+    
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -58,18 +90,36 @@ def main():
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
 
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    eventsResult = service.events().list(
-        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
-        orderBy='startTime').execute()
-    events = eventsResult.get('items', [])
+    startTime = datetime.datetime(2015,9,21).isoformat() + 'Z'
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    lastHarvest,file_id = get_last_collection_time_and_file_id()
+    file_id = file_id + 1 
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    with open('%s/zcarwile_%d.txt' % (DATA_DIR,file_id),'w') as f:    
+    
+        print('Getting Google Calendar events since last harvest')
+        print(lastHarvest)
+        eventsResult = service.events().list(
+            calendarId='primary', timeMin=lastHarvest, timeMax=now, singleEvents=True,
+            orderBy='startTime').execute()
+        events = eventsResult.get('items', [])
+    
+        if not events:
+            print('No new events found.')
+        else:
+            print('%s new events found' % (str(len(events))))
+        for event in events:
+            
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            end = event['end'].get('dateTime', event['end'].get('date'))
+            org = event['organizer'].get('displayName')
+            
+            #TODO -- do I want to add support for attendees and title?        
+            
+            f.write("%s\t%s\t%s\t%s\n" % (start, end, org, event['summary']))
+            #print("%s\t%s\t%s\t%s\n" % (start, end, org, event['summary']))
+        
+        print('Written to %s/zcarwile_%d.txt' % (DATA_DIR,file_id))
 
 
 if __name__ == '__main__':
