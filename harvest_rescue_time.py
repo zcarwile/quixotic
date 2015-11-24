19bc2afbc6998cd9de5d5f6f615570f68bbbb64c
Zach Carwile <zachary.carwile@gmail.com>
2015-11-24 15:12:21 -0500

    Adding per-day loop

--- harvest_rescue_time.py
+++ harvest_rescue_time.py
@@ -1,5 +1,7 @@
 import requests
 import pickle
+import sys
+import datetime
 
 from qlib import get_last_collection_time_and_file_id
 
@@ -16,21 +18,37 @@ with open("rescue_time_key") as f:
     # TODO: Parameterize from outside
     
     lastHarvest,file_id = get_last_collection_time_and_file_id(DATA_DIR_RESCUE_TIME)
-    date = "2015-11-10"
+    date = datetime.datetime.strptime(lastHarvest[0:10], "%Y-%m-%d")
+    now = datetime.datetime.now()
     
-    url = "https://www.rescuetime.com/anapi/data?key=%s&perspective=interval&restrict_kind=%s&interval=%s&restrict_begin=%s&restrict_end=%s&format=%s" % (key,kind,interval,date,date,format)
+    print("Last Rescue Time Harvest: ")
+    print(date)
     
-    response = requests.get(url)
+    if (date < now):
+        print("\nCalling RT API")
+    else:
+        print("Nothing to harvest")
     
-    #print("Pickling RT WS response")
-    #pickle.dump(response.json(), open('sample_rescue_time.p','wb'))
-    #data = pickle.load(open('sample_rescue_time.p','rb'))  
+    while (date < now):    
+        str_date = str(date)[0:10]  
+        print(str_date)        
+  
+        url = "https://www.rescuetime.com/anapi/data?key=%s&perspective=interval&restrict_kind=%s&interval=%s&restrict_begin=%s&restrict_end=%s&format=%s" % (key,kind,interval,str_date,str_date,format)
+        
+        response = requests.get(url)
+        
+        #print("Pickling RT WS response")
+        #pickle.dump(response.json(), open('sample_rescue_time.p','wb'))
+        #data = pickle.load(open('sample_rescue_time.p','rb'))  
+        
+        data = response.json()
+        rows = data['rows']
     
-    data = response.json()
-    rows = data['rows']
-
-with open('%s/zcarwile_%s.txt' % (DATA_DIR_RESCUE_TIME,date),'w') as f:    
-    for row in rows:
-        if int(row[1]) > 60: 
-            f.write('%s\t%s\t%s\t%s\n' %  (row[0],row[1],row[3],row[4]))
+        with open('%s/zcarwile_%s.txt' % (DATA_DIR_RESCUE_TIME,str_date),'w') as f:    
+            for row in rows:
+                if int(row[1]) > 60: 
+                    f.write('%s\t%s\t%s\t%s\n' %  (row[0],row[1],row[3],row[4]))
+                    
+        date = date + datetime.timedelta(days=1)
+    
             
